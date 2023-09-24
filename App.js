b6330f0cdf7110c61c4202ce69f79cbef2facddc
Muhammad Talal Iqbal <108578162+TalalIqbal@users.noreply.github.com>
2023-09-24 14:07:11 +0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import DarkMode from './Components/DarkMode';
+import Login from './Components/DarkMode';
+import Navbar from './Components/Navbar';
+
+
+function App() {
+  return (
+  <>
+  <Navbar/>
+  <DarkMode/>
+  </>
+  );
+}
+
+export default App;
